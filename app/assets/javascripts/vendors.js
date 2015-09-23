3d882d7b8c5ed4ab707f7c73b8fb51b84f3d7210
jaebird123 <jeaton@mitre.org>
2015-09-23 11:20:12 -0400

    made dynamic addition of POCs to the add vendrs page

--- app/assets/javascripts/vendors.js
+++ app/assets/javascripts/vendors.js
@@ -3,8 +3,38 @@
 
 jQuery(document).ready(function($) {
 
+	/* NOTE: please remove this function later along with all 'clickable-row' classes */
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
+    });
+
+    poc_index = 0;
+
+    $("#add_vendor_form")
+
+	    .on('click', '.add_button', function() {
+
+	    	poc_index++;
+
+	    	var template = $('#poc_template');
+
+	    	var clone = template.clone()
+	    						.removeClass('hide')
+	    						.removeAttr('id')
+	    						.attr('data-poc-index', poc_index)
+	    						.insertBefore(template);
+
+	    	clone.find('[name = "name"]').attr('name', 'poc[' + poc_index + '].name').end()
+	    		 .find('[name = "contact_type"]').attr('name', 'poc[' + poc_index + '].contact_type').end()
+	    		 .find('[name = "email"]').attr('name', 'poc[' + poc_index + '].email').end()
+	    		 .find('[name = "phone_number"]').attr('name', 'poc[' + poc_index + '].phone_number').end()
+
+	    })
+
+	    .on('click', '.remove_button', function() {
+
+	    	$(this).parent().parent('.form-group').remove();
+
     });
 
 });
