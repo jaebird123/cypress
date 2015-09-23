d0dc7a1cb052c2923c0b67d564523bf2fc2c82d3
jaebird123 <jeaton@mitre.org>
2015-09-23 14:20:41 -0400

    added relationship between vendors and products. added a few fields to test each model

---
+++ app/models/vendor.rb
@@ -0,0 +1,34 @@
+class Vendor
+
+	include Mongoid::Document
+	include Mongoid::Attributes::Dynamic
+	include Mongoid::Timestamps
+
+	has_many :products, dependent: :destroy
+
+	field :name, type: String
+	field :vendor_id, type: String
+	field :url, type: String
+	field :address, type: String
+	field :state, type: String
+	field :zip, type: String
+	field :poc, type: String
+	field :tel, type: String
+
+	validates_presence_of :name
+
+	# Methods should be used only for testing
+
+	def failing_products
+		return 5
+	end
+
+	def passing_products
+		return 3
+	end
+
+	def incomplete_products
+		return 1
+	end
+
+end
