9a9b9dcfb53bfb5e1baaab4c1b179ac221b70f66
jaebird123 <jeaton@mitre.org>
2015-09-28 14:47:42 -0400

    Added uniqueness check for vendor names. Added notifications for successful creation of vendor, editing of a vendor, and warnings for same vendor name

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,6 +2,10 @@ class VendorsController < ApplicationController
 
   before_filter :find_vendor, :only => [:show, :update]
 
+  # rescue_from Mongoid::Errors::Validations do
+  #    render :template => "vendors/edit"
+  # end
+
   def index
     @vendors = Vendor.all.order(updated_at: :desc)
     respond_to do |f|
@@ -26,7 +30,15 @@ class VendorsController < ApplicationController
   def create
     remove_empty_pocs
     @vendor = Vendor.new(get_vendor_permissions)
-    @vendor.save!
+    begin
+      @vendor.save!
+      flash[:notice] = "Vendor '" + @vendor.name + "' was successfully created."
+      flash[:notice_type] = "success"
+    rescue Mongoid::Errors::Validations
+      flash[:notice] = "Vendor name '" + @vendor.name + "' is already taken. Please try another name."
+      redirect_to '/vendors/new'
+      return
+    end
     respond_to do |f|
       f.json { redirect_to vendor_url(@vendor) }
       f.html { redirect_to root_path }
@@ -41,10 +53,19 @@ class VendorsController < ApplicationController
     @vendor.unset(:pocs)
     remove_empty_pocs
     @vendor.update_attributes(get_vendor_permissions)
-    unless @vendor.save
-      # rendor template? check why this would be the case
-      render :template => 'vendor/edit'
+    begin
+      @vendor.save!
+      flash[:notice] = "Vendor '" + @vendor.name + "' was edited."
+      flash[:notice_type] = "info"
+    rescue Mongoid::Errors::Validations
+      flash[:notice] = "Vendor name '" + @vendor.name + "' is already taken. Please try another name."
+      redirect_to action: "edit", id: @vendor.id
+      return
     end
+    # unless @vendor.save
+    #   # rendor template? check why this would be the case
+    #   render :template => 'vendors/edit'
+    # end
     redirect_to '/'
   end
 
