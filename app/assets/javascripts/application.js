18eee4d8915fd33094bc2a9ed43adc8c1c6bc8f4
thecristen <cristen@outlook.com>
2015-09-29 10:25:05 -0400

    Try stickyRows jquery plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
 
 //= require bootstrap-sprockets
 //= require bootstrap
+//= require stickyRows
+
+//= require_tree .
