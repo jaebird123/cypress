c4a45b714bf643029465db2ca5ee25c158047c1d
jaebird123 <jeaton@mitre.org>
2015-09-30 19:22:52 -0400

    Fixed cancel button on add/edit vendors page. Rendor instead of redirect when vendor name is already taken so that information does not need to be reentered. Removed need to unset POC's because POC's no longer duplicate.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -23,24 +23,21 @@ class VendorsController < ApplicationController
 
   def new
     @vendor = Vendor.new
-    # @vendor.pocs.build(name: "Bob")
-    # @vendor.pocs.build(name: "Tim")
   end
 
   def create
     remove_empty_pocs
-    @vendor = Vendor.new(get_vendor_permissions)
+    @vendor = Vendor.new(vendor_params) # NOTE: change to create after changing error handeling
     begin
       @vendor.save!
-      flash[:notice] = "Vendor '" + @vendor.name + "' was successfully created."
-      flash[:notice_type] = "success"
+      set_flash_vendor_comment(@vendor.name, "success", "created")
     rescue Mongoid::Errors::Validations
-      flash[:notice] = "Vendor name '" + @vendor.name + "' is already taken. Please try another name."
-      redirect_to '/vendors/new'
+      set_flash_name_taken(@vendor.name, "warning")
+      render :new
       return
     end
     respond_to do |f|
-      f.json { redirect_to vendor_url(@vendor) }
+      f.json { redirect_to vendor_url(@vendor) } # TODO: this deals with API
       f.html { redirect_to root_path }
     end
   end
@@ -50,38 +47,48 @@ class VendorsController < ApplicationController
   end
 
   def update
-    @vendor.unset(:pocs)
+    # @vendor.unset(:pocs)
     remove_empty_pocs
-    @vendor.update_attributes(get_vendor_permissions)
+    @vendor.update_attributes(vendor_params)
     begin
       @vendor.save!
-      flash[:notice] = "Vendor '" + @vendor.name + "' was edited."
-      flash[:notice_type] = "info"
+      set_flash_vendor_comment(@vendor.name, "info", "edited")
     rescue Mongoid::Errors::Validations
-      flash[:notice] = "Vendor name '" + @vendor.name + "' is already taken. Please try another name."
-      redirect_to action: "edit", id: @vendor.id
+      set_flash_name_taken(@vendor.name, "warning")
+      render :edit
       return
     end
     # unless @vendor.save
     #   # rendor template? check why this would be the case
     #   render :template => 'vendors/edit'
     # end
-    redirect_to '/'
+    redirect_to root_path
   end
 
   private
 
-  def find_vendor
-    @vendor = Vendor.find(params[:id])
-  end
+    def find_vendor
+      @vendor = Vendor.find(params[:id])
+    end
 
-  def get_vendor_permissions
-    params[:vendor].permit(:name, :vendor_id, :name, :vendor_id, :url, :address, :state, :zip, :pocs_attributes => [:name, :email, :phone, :contact_type])
-  end
+    def vendor_params
+      params[:vendor].permit(:name, :vendor_id, :name, :vendor_id, :url, :address, :state, :zip, :pocs_attributes => [:id, :name, :email, :phone, :contact_type, :_destroy])
+    end
 
-  def remove_empty_pocs
-    temp = params[:vendor][:pocs_attributes]
-    params[:vendor][:pocs_attributes].delete_if { |i| !(temp[i].has_key?("name")) || (temp[i]["name"] == "" && temp[i]["phone"] == "" && temp[i]["email"] == "" && temp[i]["contact_type"] == "") }
-  end
+    def remove_empty_pocs
+      temp = params[:vendor][:pocs_attributes]
+      params[:vendor][:pocs_attributes].delete_if { |i| !(temp[i].has_key?("name")) || (temp[i]["name"] == "" && temp[i]["phone"] == "" && temp[i]["email"] == "" && temp[i]["contact_type"] == "") }
+    end
+
+    def set_flash_name_taken(vendor_name, notice_type)
+      flash[:notice] = "Vendor name '" + vendor_name + "' is already taken. Please try another name."
+      flash[:notice_type] = notice_type
+    end
+
+    # action_type (string) describes what just happended to the vendor. should be past tense
+    def set_flash_vendor_comment(vendor_name, notice_type, action_type)
+      flash[:notice] = "Vendor '" + vendor_name + "' was " + action_type + "."
+      flash[:notice_type] = notice_type
+    end
   
 end
